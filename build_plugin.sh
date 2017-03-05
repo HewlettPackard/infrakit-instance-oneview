42f4f907b1b5e490db03981be7ebab48a535b7c0
Daniel Finneran <daniel.jam.finneran@hpe.com>
2017-03-05 16:22:14 +0000

    Created a build script
    Should be a make file, once i’ve fixed it.

--- build_plugin.sh
+++ build_plugin.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-wget http://www.digip.org/jansson/releases/jansson-2.9.tar.gz
+curl -O http://www.digip.org/jansson/releases/jansson-2.9.tar.gz
 tar -xvzf jansson-2.9.tar.gz
+mkdir -p ./lib
 cd jansson-2.9
 ./configure; make
 cp ./src/jansson.h ../headers
 cp ./src/jansson_config.h ../headers
 cp ./src/.libs/libjansson.a ../lib
 cd ..
-
+rm  jansson-2.9.tar.gz
 echo ""
 echo ""
 echo Libraries and headers added into /lib and /headers
+
+gcc ./src/*.c ./infrakit-instance-oneview.c -I./headers -L./lib -ljansson -lcurl -o infrakit-instance-oneview
+
